035f96883ebc175d81749cd1fe1d0250fb7d07af
KonstEmelyantsev <konstemel@MacBook-Pro-KonstEmelyantsev.local>
2015-09-22 11:57:35 +0300

    Initial Commit

---
+++ ServicesSector/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ServicesSector
+//
+//  Created by KonstEmelyantsev on 9/22/15.
+//  Copyright (c) 2015 KonstEmelyantsev. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ ServicesSector/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  ServicesSector
+//
+//  Created by KonstEmelyantsev on 9/22/15.
+//  Copyright (c) 2015 KonstEmelyantsev. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
